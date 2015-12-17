b28572c845b083f4b710e9299b0281ff4f61080b
kraemer-igroup <a.kraemer@i-group.de>
2015-12-17 11:49:55 +0100

    Multilanguage support
    Enable for pages_language_overlay

--- ext_tables.php
+++ ext_tables.php
@@ -19,10 +19,17 @@ $GLOBALS['TCA']['pages']['columns']['description']['config']['wizards'] = array(
 $GLOBALS['TCA']['pages']['columns']['title']['config']['wizards']['SERPWizard'] = $GLOBALS['TCA']['pages']['columns']['description']['config']['wizards']['SERPWizard'];
 $GLOBALS['TCA']['pages']['columns']['title']['config']['wizards']['SERPWizard']['params'] = array(
 	'previewSelector' => '.srs-title',
+	'overrideWithSelector' => 'input[name*=\\\[tx_seo_titletag\\\]]',
 	'maxLength' => 70,
 	'emptyValue' => 'No title'
 );
 
 $GLOBALS['TCA']['pages']['columns']['tx_seo_titletag']['config']['wizards']['SERPWizard'] = $GLOBALS['TCA']['pages']['columns']['title']['config']['wizards']['SERPWizard'];
 $GLOBALS['TCA']['pages']['columns']['tx_seo_titletag']['config']['wizards']['SERPWizard']['params']['fallbackFieldSelector'] = 'input[name*=\\\[title\\\]]';
+$GLOBALS['TCA']['pages']['columns']['tx_seo_titletag']['config']['wizards']['SERPWizard']['params']['overrideWithSelector'] = '';
+
+# Page language overlay
+$GLOBALS['TCA']['pages_language_overlay']['columns']['description']['config']['wizards'] = $GLOBALS['TCA']['pages']['columns']['description']['config']['wizards'];
+$GLOBALS['TCA']['pages_language_overlay']['columns']['title']['config']['wizards'] = $GLOBALS['TCA']['pages']['columns']['title']['config']['wizards'];
+$GLOBALS['TCA']['pages_language_overlay']['columns']['tx_seo_titletag']['config']['wizards'] = $GLOBALS['TCA']['pages']['columns']['tx_seo_titletag']['config']['wizards'];
 ?>
